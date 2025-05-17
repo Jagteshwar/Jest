ca4b216ad1e6efa86e1591de0221e1f23cd10ecb
Jagteshwar <jagteshwars3@gmail.com>
2025-05-16 22:51:42 -0400

    Async testing WIP

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function multiply(a, b){
-    return a*b;
-}
+// function multiply(a, b){
+//     return a*b;
+// }
+
+const multiply = require(".");
+
+const fetchData=(shouldFail = false) => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            if(shouldFail){
+                reject('error occured.')
+            }else{
+                resolve()
+            }
+        }, 100)
+    })
+};
 
 module.exports = multiply
